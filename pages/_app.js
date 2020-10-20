07044721d48c9cb1692d411c1433d5ed8096a9cc
Sam Finckh <samuelfinckh@gmail.com>
2020-10-20 13:40:34 +1100

    errorr Please enter the commit message for your changes. Lines starting

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import React, { Component } from 'react';
 import CardList from './card-list';
 import 'fontsource-roboto';
 
+
+
 function MyApp() {
     return (
       <div className='body-container'>
